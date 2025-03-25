97ddb354f7b68cd4a942b45499a6a4e49cc8ec90
lakshayjain <lakshay2007jain@gmail.com>
2025-03-25 19:42:15 +0530

    Initial commit

---
+++ src/test/java/com/pdfreader/Pdf_Reader/PdfReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfreader.Pdf_Reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
